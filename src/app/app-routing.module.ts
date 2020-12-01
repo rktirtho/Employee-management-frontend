44e875478475aafee67bca63d066ca5d925398eb
Rejaul Karim Tirtho <rktirtho@gmail.com>
2020-12-01 18:14:20 +0600

    router added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
+import { EmployeeListComponent } from './employee-list/employee-list.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : 'employees', component : EmployeeListComponent},
+  {path: '', redirectTo:'employees', pathMatch : 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
